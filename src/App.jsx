6ad96059bb09ea5abfd8f46ae5ffeab5a7215b69
Asir Mahmud Ishmam <asirmahmud2@gmail.com>
2026-03-30 13:12:08 +0600

    Tailwind and DaisiUI set up

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+        <p className='font-bold text-8xl'>Hello WOrld!</p>
+        <button className='btn btn-primary'>Button</button>
+    </>
+  )
+}
+
+export default App
