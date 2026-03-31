2124605b4d9f86a805310243dee722eda30c2e19
Asir Mahmud Ishmam <asirmahmud2@gmail.com>
2026-03-31 20:14:16 +0600

    Product design

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,30 @@
+import { Suspense } from 'react'
 import './App.css'
 import Getstarted from './Components/Homepage/Details/Getstarted'
 import Pricing from './Components/Homepage/Details/Pricing'
 import Hero from './Components/Homepage/Hero/Hero'
 import Rating from './Components/Homepage/Hero/Rating'
 import Navbar from './Components/Homepage/Navbar/Navbar'
+import Products from './Components/Homepage/Products/Products'
+
+const fetchPlayer = async () => {
+  const res = await fetch("/product.json");
+  return res.json();
+}
 
 function App() {
+  const productsPromise = fetchPlayer();
 
   return (
     <>
-        <Navbar></Navbar>
-        <Hero></Hero>
-        <Rating></Rating>
-        <Getstarted></Getstarted>
-        <Pricing></Pricing>
+      <Navbar></Navbar>
+      <Hero></Hero>
+      <Rating></Rating>
+      <Getstarted></Getstarted>
+      <Pricing></Pricing>
+      <Suspense>
+        <Products productsPromise={productsPromise}></Products>
+      </Suspense>
     </>
   )
 }
---
+++ src/Components/Homepage/Products/Products.jsx
@@ -0,0 +1,37 @@
+import React from 'react';
+import { use } from 'react';
+
+const Products = ({ productsPromise }) => {
+    const products = use(productsPromise);
+
+    return (
+        <div>
+            <div className='container mx-auto p-20 space-y-3'>
+                <div className="text-center">
+                    <h2 className='text-4xl font-bold mb-3'>Simple, Transparent Pricing</h2>
+                    <p className='text-gray-400 '>Choose the plan that fits your needs. Upgrade or downgrade anytime.</p>
+                </div>
+                <div className="flex justify-center mt-10">
+                    <div className="flex bg-gray-200 rounded-full p-1 w-fit">
+                        <button className="btn px-6 py-2 rounded-full text-white bg-linear-to-r from-[#4F39F6] to-[#9514FA] shadow-md">
+                            Products
+                        </button>
+                        <button className="btn px-6 py-2 rounded-full text-gray-700">
+                            Cart (2)
+                        </button>
+                    </div>
+                </div>
+
+                <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5 mt-10'>
+                    {
+                        products.map(product => {
+                            return <h1 key={product.price}>{product.title}</h1>
+                        })
+                    }
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Products;
